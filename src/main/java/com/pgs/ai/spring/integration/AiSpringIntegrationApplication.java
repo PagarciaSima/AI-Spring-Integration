1b0fc3c71b780af920e47a0a6606eac08b76bac5
pagarciaSim <pablo.garciasimavilla@gmail.com>
2025-07-26 16:01:21 +0200

    First commit

---
+++ src/main/java/com/pgs/ai/spring/integration/AiSpringIntegrationApplication.java
@@ -0,0 +1,13 @@
+package com.pgs.ai.spring.integration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiSpringIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiSpringIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pgs/ai/spring/integration/AiSpringIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgs.ai.spring.integration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiSpringIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
